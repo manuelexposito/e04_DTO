3c4ffe85c2a23e72dd685b8577738f5fadd8f4ab
Pablo <pabloseguravelasco@gmail.com>
2021-09-29 17:43:15 +0200

    Al commit ese del email ni caso

--- e02/src/main/java/com/salesianostriana/e04_e02/demo/pojo/Producto.java
+++ e02/src/main/java/com/salesianostriana/e04_e02/demo/pojo/Producto.java
@@ -25,21 +25,11 @@ public class Producto {
     private String desc;
     private double pvp;
     private Collection<String>imagenes;
-    private String email;
+
 
     @ManyToOne
     Categoria categoria;
 
-    public Producto(String nombre, String desc, double pvp, Collection<String> imagenes, String email, Categoria categoria) {
-        this.nombre = nombre;
-        this.desc = desc;
-        this.pvp = pvp;
-        this.imagenes = imagenes;
-        this.email = email;
-        this.categoria = categoria;
-    }
-
-    //Constructor sin email para crear un producto en la clase converter
     public Producto(String nombre, String desc, double pvp, Collection<String> imagenes, Categoria categoria) {
         this.nombre = nombre;
         this.desc = desc;
@@ -47,4 +37,6 @@ public class Producto {
         this.imagenes = imagenes;
         this.categoria = categoria;
     }
+
+
 }
