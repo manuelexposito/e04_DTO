66651ad93f7585d9bb1fd4763cf74768fde39559
ManuelExposito <manuelexposito4d@gmail.com>
2021-09-29 12:50:45 +0200

    Ejercicio 01 subido

---
+++ e01/src/test/java/com/salesianostriana/e04_e01/demo/E04DtoE01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.e04_e01.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E04DtoE01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
